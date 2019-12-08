d6a38fced4ab704caee9eb003aa56f5081e2c3e3
dorinma <dorinma@post.bgu.ac.il>
2019-12-08 22:15:53 +0200

    testing

--- src/test/java/bgu/spl/mics/FutureTest.java
+++ src/test/java/bgu/spl/mics/FutureTest.java
@@ -1,12 +1,21 @@
 package bgu.spl.mics;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FutureTest {
+
+    //Future<T> future;
+
     @BeforeEach
     public void setUp(){
+        //future = new Future<T>();
+    }
+
+    @AfterEach
+    public void tearDown(){
 
     }
 
@@ -14,5 +23,17 @@ public class FutureTest {
     public void test(){
         //TODO: change this test and add more tests :)
         fail("Not a good test");
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void resolve() {
+    }
+
+    @Test
+    void isDone() {
     }
 }
--- src/test/java/bgu/spl/mics/InventoryTest.java
+++ src/test/java/bgu/spl/mics/InventoryTest.java
@@ -1,21 +1,51 @@
 package bgu.spl.mics;
 
+import bgu.spl.mics.application.passiveObjects.Inventory;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class InventoryTest {
-    @BeforeEach
-    public void setUp(){
 
+    Inventory inv;
+
+    @BeforeEach
+    public void setUp() {
+        inv = new Inventory();
+        inv.load(new String[]{"Sky Hook", "X-ray Glasses"});
+    }
+
+    @AfterEach
+    public void tearDown() {
+        inv = null;
     }
 
     @Test
-    public void test(){
-        //TODO: change this test and add more tests :)
-        //fail("Not a good test");
-        assertEquals(1,1);
+    public void test_GetItem1() {
+        boolean exists = inv.getItem("Sky Hook");
+        assertTrue(exists);
+    }
+
+    @Test
+    public void test_GetItem2() {
+        boolean exists = inv.getItem("X-ray Glasses");
+        assertTrue(exists);
+    }
+
+    @Test
+    public void test_GetItem3() {
+        boolean exists = inv.getItem("Dagger shoe");
+        assertFalse(exists);
+    }
+
+    @Test
+    public void test_GetInstance() {
+        Inventory inv2 = new Inventory();
+        assertSame(inv, inv2);
     }
 }
